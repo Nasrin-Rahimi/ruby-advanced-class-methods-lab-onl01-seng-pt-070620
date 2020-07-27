e5cebf459db5360c96da0b7dfe19bcb7e2648e76
Nasrin Rahimi <77.nasrin@gmail.com>
2020-07-27 11:38:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,6 +50,7 @@ end
 
 song_1 = Song.find_or_create_by_name("Blank Space")
 song_2 = Song.find_or_create_by_name("Alank Space")
+Song.alphabetical
 binding.pry
 
 the_middle = Song.create_by_name("The Middle")
