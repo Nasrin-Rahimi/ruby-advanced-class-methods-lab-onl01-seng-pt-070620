b721dc7bff96c6ba3cefdc39d0ee90404fd28370
Nasrin Rahimi <77.nasrin@gmail.com>
2020-07-27 11:04:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,11 @@ require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+  
+  def self.create
+    song = self.new
+    self.class.all << self
+  end
 
   def self.all
     @@all
